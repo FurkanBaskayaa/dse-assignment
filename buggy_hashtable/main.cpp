d7580a8bc430fb73c01d0a3defd6f31c7d5772da
unknown <ge83rob@mytum.de>
2023-07-18 23:17:27 +0200

    submit

--- buggy_hashtable/main.cpp
+++ buggy_hashtable/main.cpp
@@ -25,61 +25,103 @@ inline uint64_t hashKey(uint64_t k) {
 }
 
 struct Hashtable {
-   struct Entry {
-      uint64_t key;
-      uint64_t value;
-      Entry* next;
-   };
+    struct Entry {
+        uint64_t key;
+        uint64_t value;
+        Entry* next;
+    };
 
-   uint64_t htSize;
-   uint64_t mask;
-   Entry** ht;
+    uint64_t htSize;
+    uint64_t mask;
+    Entry** ht;
+    Entry** ht_end;
 
-   Hashtable(uint64_t size) {
-      htSize = 1ull << ((8*sizeof(uint64_t)) - __builtin_clzl(size)); // next power of two
-      mask = htSize - 1;
-      ht = reinterpret_cast<Entry**>(allocZeros(sizeof(uint64_t) * htSize));
-   }
+    Hashtable(uint64_t size) {
+        htSize = 1ull << ((8*sizeof(uint64_t)) - __builtin_clzl(size)); // next power of two
+        mask = htSize - 1;
+        ht = reinterpret_cast<Entry**>(allocZeros(sizeof(uint64_t) * htSize));
+        ht_end = reinterpret_cast<Entry**>(allocZeros(sizeof(uint64_t) * htSize));
+    }
 
-   ~Hashtable() {
-      munmap(ht, htSize);
-   }
+    ~Hashtable() {
+        for (uint64_t i=0; i<htSize; i++) {
+            this->erase(i);
+        }
+        munmap(ht, htSize * sizeof(Entry*));
+        munmap(ht_end, htSize * sizeof(Entry*));
+    }
 
-   Entry* lookup(uint64_t key) {
-      for (Entry* e=ht[hashKey(key)]; e; e=e->next)
-         if (e->key==key)
-            return e;
-      return nullptr;
-   }
+    Entry* lookup(uint64_t key) {
+        for (Entry* e=ht[hashKey(key) & mask]; e; e=e->next)
+            if (e->key==key)
+                return e;
+        return nullptr;
+    }
 
-   bool insert(uint64_t key, uint64_t value) {
-      Entry* e = lookup(key);
-      if (e) {
-         e->value = value;
-         return false;
-      } else {
-         uint64_t pos = hashKey(key) & mask;
-         Entry* newEntry = new Entry();
-         ht[pos] = newEntry;
-         newEntry->key = key;
-         newEntry->value = value;
-         newEntry->next = ht[pos];
-         return true;
-      }
-   }
-
-   bool erase(uint64_t key) {
-      uint64_t pos = hashKey(key) & mask;
-      Entry** ePtr = &ht[pos];
-      while (Entry* e=(*ePtr)) {
-         if (e->key==key) {
-            delete e;
+    bool insert(uint64_t key, uint64_t value) {
+        Entry* e = lookup(key);
+        if (e) {
+            e->value = value;
+            return false;
+        } else {
+            uint64_t pos = hashKey(key) & mask;
+            Entry* newEntry = new Entry();
+            newEntry->key = key;
+            newEntry->value = value;
+            newEntry->next = NULL;
+            if(!ht[pos]){
+                ht[pos] = newEntry;
+                ht_end[pos] = newEntry;
+            } else {
+                ht_end[pos]->next = newEntry;
+                ht_end[pos] = ht_end[pos]->next;
+            }
             return true;
-         }
-         ePtr = &e->next;
-      }
-      return false;
-   }
+        }
+    }
+
+    bool erase(uint64_t key) {
+        uint64_t pos = hashKey(key) & mask;
+        Entry* prev = ht[pos];
+        Entry* curr = ht[pos];
+        while (curr) {
+            if (curr->key==key) {
+                //One element chain
+                if (curr == ht[pos] && curr == ht_end[pos]) {
+                    ht[pos] = nullptr;
+                    ht_end[pos] = nullptr;
+                    delete curr;
+                    return true;
+                }
+                    //Delete head
+                else if (curr == ht[pos]) {
+                    ht[pos] = curr->next;
+                    delete curr;
+                    return true;
+                }
+                    //Delete tail
+                else if (curr == ht_end[pos]) {
+                    prev->next = nullptr;
+                    ht_end[pos] = prev;
+                    delete curr;
+                    return true;
+                }
+                    //Delete from middle
+                else {
+                    prev->next = curr->next;
+                    delete curr;
+                    return true;
+                }
+            }
+            if(prev == curr){
+                curr = curr->next;
+            } else {
+                prev = curr;
+                curr = curr->next;
+            }
+        }
+        return false;
+    }
 };
 
 int main() {
